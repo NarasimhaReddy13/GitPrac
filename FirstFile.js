901a067d66c5508754f085552a47ca64e5ae1f17
NarasimhaReddy13 <narasimha@react.practice.com>
2021-08-04 20:50:59 +0530

    FIrst IIFE

---
+++ FirstFile.js
@@ -0,0 +1,18 @@
+// git init --- ls -a
+
+var userName = "Bill";
+
+(function (name) {
+
+  function display(name){
+
+      console.log(name);
+    }
+
+    display(name);
+})(userName); 
+
+// git status
+
+
+// git add . --- or git add fileName/filePath
